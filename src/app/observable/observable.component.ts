d9acbd123cc752eb4a218e5f6d1cfea525e6424a
volcovpaul98 <44445910+volcovpaul98@users.noreply.github.com>
2018-12-12 21:12:43 +0200

    dgv d

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { of, observable, fromEvent } from 'rxjs';
+import { of, observable, fromEvent, Subscription } from 'rxjs';
+import { User } from 'src/classes/user';
+import { map, filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-observable',
@@ -7,16 +10,64 @@ import { of, observable, fromEvent } from 'rxjs';
   styleUrls: ['./observable.component.css']
 })
 export class ObservableComponent implements OnInit {
-
+  user: User[];
+  Subscription: Subscription;
   constructor() {
     this.simpleObservable();
+    this.schoolUsers();
   }
 
+  schoolUsers() {
+    var user1 = new User();
+    user1.username = "Andrew";
+    user1.email = "andrew23@gmail.com";
+    user1.isActive = true;
+
+    var user2 = new User();
+    user2.username = "Ramona";
+    user2.email = "ramona68@yahoo.com";
+    user2.isActive = false;
+
+    var user3 = new User();
+    user3.username = "Borian";
+    user3.email = "Boriangeo@gmail.com";
+    user3.isActive = false;
+
+    var user4 = new User();
+    user4.username = "Carmen";
+    user4.email = "carmen.elisabeth@gmail.com";
+    user4.isActive = true;
+
+    var observable = of(user1, user2, user3, user4);
+    var nameUsers = map((u: User) => u.username + "(" + u.email + ")");
+    var activeUsers = filter((u: User) => u.isActive == true)
+
+    var subscribeUsers = {
+      next: x => console.log(x),
+      error: err => { console.log(err.message) },
+      complete: () => { console.log("completed") }
+    }
+    observable.pipe(activeUsers, nameUsers).subscribe(subscribeUsers);
+  }
   ngOnInit() {
-    this.handlerKey();
+
+    this.Subscription = this.handlerKey();
+
+    var checkBox = document.getElementById("clearonESC") as HTMLInputElement;
+    var observable = fromEvent(checkBox, 'change');
+
+    observable.subscribe((event: any) => {
+      if (event.target.checked) {
+        this.Subscription = this.handlerKey();
+      }
+      else {
+        this.Subscription.unsubscribe();
+      }
+    })
+
   }
   simpleObservable() {
-    var observable = of(1, 2, 3);
+    var observable = of(1, 2, 3)
     var HandlerObservable1 = {
       next: x => console.log(x),
       error: err => console.log(err.message),
@@ -32,16 +83,23 @@ export class ObservableComponent implements OnInit {
   }
 
 
-  handlerKey() {
+  handlerKey(): Subscription {
     var nameInput = document.getElementById('name') as HTMLInputElement;
     var observable1 = fromEvent(nameInput, 'keydown');
-    observable1.subscribe((event: KeyboardEvent) => {
-      console.log(event);
+    return observable1.subscribe((event: KeyboardEvent) => {
+      // console.log(event);
       if (event.keyCode === 27) {
         nameInput.value = "";
       }
+      observable1.subscribe((event: KeyboardEvent) => {
+        if (event.keyCode == 13) {
+          alert(nameInput.value);
+        }
+      })
 
     })
+
   }
+
 
 }
